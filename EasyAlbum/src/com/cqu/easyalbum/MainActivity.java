c0ee2d7e4cf073f4b4e122f19c0309614822bb02
simpher <hezhen127287@163.com>
2014-06-12 11:07:07 +0800

    init

---
+++ EasyAlbum/src/com/cqu/easyalbum/MainActivity.java
@@ -0,0 +1,13 @@
+package com.cqu.easyalbum;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.layout_main_activity);
+	}
+}
